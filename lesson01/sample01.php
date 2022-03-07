6b118ae7ea41ff9b240f04a93766b86e195742de
root <root@os3-357-11840.vs.sakura.ne.jp>
2022-03-07 17:09:34 +0900

    initial commit

---
+++ lesson01/sample01.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+		echo "I\"m \"studying\"";  //エスケープシーケンス
+
+		//10：計算
+		echo 123+2*5;
+		echo 156*5;
+
+		//11 現在時刻の表示
+		date_default_timezone_set("Asia/Tokyo");
+		echo date("G時 i分 s秒\n");
+
+		//12 文字列連結
+		date_default_timezone_set("Asia/Tokyo");
+		echo "現在は、".date("G時 i分 s秒\n")."です。";
+
+		//13 オブジェクトで日付を扱う
+		$today = new DateTime();
+		$today->setTimezone(new DateTimeZone("Asia/Tokyo"));
+		echo "現在の時刻は", $today->format("G時 i分 s秒\n"), "です。";
+	?> 
+	
+</body>
+</html>
